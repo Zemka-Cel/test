b59ff781405ddb3d35426892026986ab4afad049
Zemka-Cel <azemina.celebic@stu.ssst.edu.ba>
2019-10-18 23:26:13 +0200

    dodana slika navbar

--- src/views/components/nav.js
+++ src/views/components/nav.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../style/navstyle.css';
+import Profilna from '../assets/profilna.png';
 
 class Nav extends React.Component {
 
@@ -26,6 +27,7 @@ class Nav extends React.Component {
                         </ul>
 
                     </div>
+                    <img className="pt-1" src={Profilna}></img>
                 </nav>
             </div>
         )
