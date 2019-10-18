b5ec2f6fa73d0e9b5fa78fdc52c31d0b5d954e1f
Zemka-Cel <azemina.celebic@stu.ssst.edu.ba>
2019-10-18 23:18:56 +0200

    popravka

--- src/views/components/nav.js
+++ src/views/components/nav.js
@@ -11,7 +11,7 @@ class Nav extends React.Component {
                     <button className="navbar-toggler" type="button" data-toggle="collapse"
                             data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
-                        <span className=" "></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
